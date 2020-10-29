6354f8fc993b661152f647f5b004dbb054331bf4
David Davini <daviddavini@Davids-MBP.surewest.net>
2020-10-29 14:31:26 -0700

    half move

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
+from pygame import Vector2
 
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 800
 FPS = 60
 
 def move_vector(pressed):
-    
+    dir = Vector2(0,0)
+    if pressed[pygame.K_w]:
+        dir += Vector2(0,1)
+    if pressed[pygame.K_d]:
+        dir -= Vector2(0,-1)
 
 pygame.init()
 
